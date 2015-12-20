f5a0e3f224588e8369095ef13018c411f267fcab
Kostadin Golev <kotseto@gmail.com>
2015-12-20 23:12:28 +0200

    Initial commit is initial

---
+++ src/main/java/hello/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().write("Hello World!".getBytes());
+    }
+}
